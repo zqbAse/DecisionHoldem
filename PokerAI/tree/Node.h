3e644299d69fa539e747cdd86f2a254e99fe93c8
zqbAse <1211455492@qq.com>
2022-05-17 20:40:08 +0800

    Update Node.h

--- PokerAI/tree/Node.h
+++ PokerAI/tree/Node.h
@@ -19,50 +19,44 @@
 #include <assert.h>
 #include <exception>
 using namespace std;
-struct strategy_node {		//info set node 
-	//bool vis;
+struct strategy_nodes {		//info set node 
 	int action_len = 0;
-	unsigned char* actionstr ;
-	double* regret;
-	//float* cfv1;
-	//float* cfv2;
-	double* averegret;
-	strategy_node* actions ;
-	strategy_node() :action_len(0){}
-	strategy_node* findnode(unsigned char action) {
+	unsigned char actionstr;
+	int regret, averegret[3];
+	strategy_nodes* actions;
+	strategy_nodes() :action_len(0), regret(0) {}
+	strategy_nodes* findnode(unsigned char action) {
 		for (int i = 0; i < action_len; i++) {
-			if (*(actionstr + i) == action)
+			if (actions[i].actionstr == action)
 				return (actions + i);
 		}
 		throw std::exception();
 	}
 	int findindex(unsigned char action) {
 		for (int i = 0; i < action_len; i++) {
-			if (*(actionstr + i) == action)
+			if (actions[i].actionstr == action)
 				return i;
 		}
 		throw std::exception();
 	}
-	void init_child(unsigned char* action_str, int _action_len) {
-		actionstr = action_str;
-		actions = new strategy_node[_action_len]();
-		regret = new double[_action_len] {0};
-		//cfv1 = new float[_action_len] {0};
-		averegret = new double[_action_len] {0};
+	void init_child(unsigned char action_str[], int _action_len) {
+
+		actions = new strategy_nodes[_action_len]();
+		for (int i = 0; i < _action_len; i++)
+			actions[i].actionstr = action_str[i];
 		action_len = _action_len;
-		
+
 	}
 	void init_chance_node(int initlen) {
-
-		actions = new strategy_node[initlen]();
+		actions = new strategy_nodes[initlen]();
 		action_len = initlen;
 	}
 };
-void calculate_strategy(double* oriregret, int len, double sigma[]) {
+void calculate_oppstrategy(strategy_nodes* oriregret, int len, double sigma[]) {
 	assert(len != 0);
 	int regret[12];
 	for (int i = 0; i < len; i++)
-		regret[i] = oriregret[i];
+		regret[i] = oriregret[i].regret;
 	double sum = 0;
 	for (int i = 0; i < len; i++) {
 		if (regret[i] > 0)
@@ -78,117 +72,211 @@ void calculate_strategy(double* oriregret, int len, double sigma[]) {
 		for (int i = 0; i < len; i++)
 			sigma[i] = 1.0 / len;
 }
-double calculate_strategy(double* regret, int len, int actioni) {
+void calculate_strategy(strategy_nodes* oriregret, int len, double sigma[], int idx) {
 	assert(len != 0);
+	int regret[12];
+	for (int i = 0; i < len; i++)
+		regret[i] = oriregret[i].averegret[idx];
 	double sum = 0;
 	for (int i = 0; i < len; i++) {
 		if (regret[i] > 0)
 			sum += regret[i];
 	}
-	if (sum > 0){
-		if (regret[actioni] > 0)
-			return regret[actioni] / sum;
-		else
-			return 0;
-	}
+	if (sum > 0)
+		for (int i = 0; i < len; i++)
+			if (regret[i] > 0)
+				sigma[i] = regret[i] / sum;
+			else
+				sigma[i] = 0;
 	else
-		return 1.0 / len;
+		for (int i = 0; i < len; i++)
+			sigma[i] = 1.0 / len;
 }
-double calculate_strategy(int* regret, int len, int actioni) {
+struct strategy_node {		//info set node 
+	int action_len = 0;
+	unsigned char actionstr;
+	int regret, averegret;
+	strategy_node* actions;
+	strategy_node() :action_len(0), regret(0), averegret(0) {}
+	strategy_node* findnode(unsigned char action) {
+		for (int i = 0; i < action_len; i++) {
+			if (actions[i].actionstr  == action)
+				return (actions + i);
+		}
+		throw std::exception();
+	}
+	int findindex(unsigned char action) {
+		for (int i = 0; i < action_len; i++) {
+			if (actions[i].actionstr == action)
+				return i;
+		}
+		throw std::exception();
+	}
+	void init_child(unsigned char action_str[], int _action_len) {
+
+		actions = new strategy_node[_action_len]();
+		for (int i = 0; i < _action_len; i++)
+			actions[i].actionstr = action_str[i];
+		action_len = _action_len;
+
+	}
+	void init_chance_node(int initlen) {
+		actions = new strategy_node[initlen]();
+		action_len = initlen;
+	}
+};
+void calculate_strategy(strategy_node* oriregret, int len, double sigma[]) {
 	assert(len != 0);
+	int regret[12];
+	for (int i = 0; i < len; i++)
+		regret[i] = oriregret[i].regret;
 	double sum = 0;
 	for (int i = 0; i < len; i++) {
 		if (regret[i] > 0)
 			sum += regret[i];
 	}
-	if (sum > 0) {
-		if (regret[actioni] > 0)
-			return regret[actioni] / sum;
-		else
-			return 0;
-	}
+	if (sum > 0)
+		for (int i = 0; i < len; i++)
+			if (regret[i] > 0)
+				sigma[i] = regret[i] / sum;
+			else
+				sigma[i] = 0;
 	else
+		for (int i = 0; i < len; i++)
+			sigma[i] = 1.0 / len;
+}
+double calculate_strategy(strategy_node* nodes, int len, int idx) {
+	assert(len != 0);
+	if (nodes[idx].averegret <= 0)
+		return 0;
+	double regret[12];
+	for (int i = 0; i < len; i++)
+		regret[i] = nodes[i].averegret;
+	double sum = 0;
+	for (int i = 0; i < len; i++) {
+		if (regret[i] > 0)
+			sum += regret[i];
+	}
+	if (sum == 0) {//确保后悔值一定有正数
 		return 1.0 / len;
+	}
+	if (regret[idx] > 0)
+		return regret[idx] / sum;
+	else
+		return 0;
 }
 
 struct subgame_node {		//info set node
 	bool frozen, leaf;	//leaf:深度受限叶节点，frozen：冻结这个节点策略不更新
 	int action_len = 0;
-	unsigned char* actionstr ;
-	double* regret;
-	double* ave_strategy;
+	unsigned char actionstr ;
+	double regret;
+	double ave_strategy;
 	subgame_node* actions ;
-	strategy_node* leafnode ;
-	double* expolitvalues;
-	subgame_node() :action_len(0), frozen(false), leaf(false), actions(NULL){}
+	strategy_nodes* leafnode ;
+	//double* expolitvalues;
+	subgame_node() :action_len(0), frozen(false), leaf(false), actions(NULL), regret(0), ave_strategy(0) {}
+
 	subgame_node* findnode(unsigned char action) {
 		if (action == 'k')
 			action = 'l';
 		for (int i = 0; i < action_len; i++) {
-			if (*(actionstr + i) == action)
+			if (actions[i].actionstr == action)
 				return (actions + i);
 		}
 		cout << "not this actions:" << action << endl;
 		throw exception();
 	}
+	bool findnodeexist(unsigned char action) {
+		if (action == 'k')
+			action = 'l';
+		for (int i = 0; i < action_len; i++) {
+			if (actions[i].actionstr == action)
+				return true;
+		}
+		return false;
+	}
 	int findindex(unsigned char action) {
 		if (action == 'k')
 			action = 'l';
 		for (int i = 0; i < action_len; i++) {
-			if (*(actionstr + i) == action)
+			if (actions[i].actionstr == action)
 				return i;
 		}
 		cout << "not this actions:" << action << endl;
 		throw exception();
 	}
-	void init_child(unsigned char* action_str, int _action_len) {
+	void init_child(unsigned char action_str[], int _action_len) {
 		//if (action_len == 0) {
-			actionstr = action_str;
-			ave_strategy = new double[_action_len] {0};
-			actions = new subgame_node[_action_len]();
-			regret = new double[_action_len] {0};
-			action_len = _action_len;
+		actions = new subgame_node[_action_len]();
+		for (int i = 0; i < _action_len; i++)
+			actions[i].actionstr = action_str[i];
+		action_len = _action_len;
 		//}
 	}
 	void init_chance_node(int initlen) {
 		//if (action_len == 0) {
 			actions = new subgame_node[initlen]();
-			expolitvalues = new double[initlen] {0};
+			//expolitvalues = new double[initlen] {0};
 			action_len = initlen;
 		//}
 	}
 };
-double calculate_strategy_action(int* oriregret, int len, int index) {
-	assert(len != 0 && index < len);
-	double sum = 0;
-	for (int i = 0; i < len; i++) {
-		if (oriregret[i] > 0) 
-			sum += oriregret[i];
-	}
-	if (sum == 0) {//确保后悔值一定有正数
-		return 1.0 / len;
-	}
-	//当前动作后悔值要正数
-	if (oriregret[index] > 0) 
-		return oriregret[index] / sum;
-	else
-		return 0;
-}
-double calculate_strategy_action(int* oriregret, int len, unsigned char actionstr[], int biasid , int index) {
+void calculate_avestrategy(subgame_node* node,int len, double sigma[]) {
 	assert(len != 0);
 	int regret[12];
 	for (int i = 0; i < len; i++)
-		regret[i] = oriregret[i];
+		regret[i] = node[i].ave_strategy;
+	double sum = 0;
+	for (int i = 0; i < len; i++) {
+		if (regret[i] > 0)
+			sum += regret[i];
+	}
+	if (sum > 0)
+		for (int i = 0; i < len; i++)
+			if (regret[i] > 0)
+				sigma[i] = regret[i] / sum;
+			else
+				sigma[i] = 0;
+	else
+		for (int i = 0; i < len; i++)
+			sigma[i] = 1.0 / len;
+}
+void calculate_accstrategy(subgame_node* node, int len, double sigma[]) {
+	assert(len != 0);
+	int regret[12];
+	for (int i = 0; i < len; i++)
+		regret[i] = node[i].regret;
+	double sum = 0;
+	for (int i = 0; i < len; i++) {
+		if (regret[i] > 0)
+			sum += regret[i];
+	}
+	if (sum > 0)
+		for (int i = 0; i < len; i++)
+			if (regret[i] > 0)
+				sigma[i] = regret[i] / sum;
+			else
+				sigma[i] = 0;
+	else
+		for (int i = 0; i < len; i++)
+			sigma[i] = 1.0 / len;
+}
+double calculate_accstrategy_action(subgame_node* node, int biasid, int index) {
+	assert(node->action_len != 0);
+	double regret[12];
+	for (int i = 0; i < node->action_len; i++)
+		regret[i] = node->actions[i].regret;
 	if (biasid == 1) {
-		if (actionstr[0] == 'd' && regret[0] > 0)
+		if (node->actions[0].actionstr == 'd' && regret[0] > 0)
 			regret[0] *= 5;
 	}
 	else if (biasid == 2) {
-		if (actionstr[0] == 'l') {
+		if (node->actions[0].actionstr == 'l') {
 			if (regret[0] > 0)
 				regret[0] *= 5;
 		}
-		else if (actionstr[1] == 'l') {
+		else if (node->actions[0].actionstr == 'l') {
 			if (regret[1] > 0)
 				regret[1] *= 5;
 		}
@@ -196,53 +284,47 @@ double calculate_strategy_action(int* oriregret, int len, unsigned char actionst
 			throw exception();
 	}
 	else if (biasid == 3) {
-		for (int i = 0; i < len; i++)
-			if (oriregret[i] > 0) {
-				if (actionstr[i] != 'l' && actionstr[i] != 'd')
-					regret[i] = oriregret[i] * 5;
-				else
-					regret[i] = oriregret[i];
+		for (int i = 0; i < node->action_len; i++)
+			if (regret[i] > 0) {
+				if (node->actions[i].actionstr != 'l' && node->actions[i].actionstr != 'd')
+					regret[i] *= 5;
 			}
 	}
 	else if (biasid == 4) {
-		for (int i = 0; i < len; i++)
-			if (oriregret[i] > 0) {
-				if (actionstr[i] != 'l' && actionstr[i] != 'd')
-					regret[i] = oriregret[i] * 10;
-				else
-					regret[i] = oriregret[i];
+		for (int i = 0; i < node->action_len; i++)
+			if (regret[i] > 0) {
+				if (node->actions[i].actionstr != 'l' && node->actions[i].actionstr != 'd')
+					regret[i] *= 10;
 			}
 	}
 	double sum = 0;
-	for (int i = 0; i < len; i++) {
+	for (int i = 0; i < node->action_len; i++) {
 		if (regret[i] > 0)
 			sum += regret[i];
 	}
 	if (sum == 0) {//确保后悔值一定有正数
-		return 1.0 / len;
+		return 1.0 / node->action_len;
 	}
 	if (regret[index] > 0)
 		return regret[index] / sum;
 	else
 		return 0;
 }
-double calculate_strategy_action(double* oriregret, int len, unsigned char actionstr[], int biasid, int index) {
-	assert(len != 0);
-	if (oriregret[index] <= 0)
-		return 0;
+double calculate_avestrategy_action(subgame_node* node, int biasid, int index) {
+	assert(node->action_len != 0);
 	double regret[12];
-	for (int i = 0; i < len; i++)
-		regret[i] = oriregret[i];
+	for (int i = 0; i < node->action_len; i++)
+		regret[i] = node->actions[i].ave_strategy;
 	if (biasid == 1) {
-		if (actionstr[0] == 'd' && regret[0] > 0)
+		if (node->actions[0].actionstr == 'd' && regret[0] > 0)
 			regret[0] *= 5;
 	}
 	else if (biasid == 2) {
-		if (actionstr[0] == 'l') {
+		if (node->actions[0].actionstr == 'l') {
 			if (regret[0] > 0)
 				regret[0] *= 5;
 		}
-		else if (actionstr[1] == 'l') {
+		else if (node->actions[0].actionstr == 'l') {
 			if (regret[1] > 0)
 				regret[1] *= 5;
 		}
@@ -250,30 +332,26 @@ double calculate_strategy_action(double* oriregret, int len, unsigned char actio
 			throw exception();
 	}
 	else if (biasid == 3) {
-		for (int i = 0; i < len; i++)
-			if (oriregret[i] > 0) {
-				if (actionstr[i] != 'l' && actionstr[i] != 'd')
-					regret[i] = oriregret[i] * 5;
-				else
-					regret[i] = oriregret[i];
+		for (int i = 0; i < node->action_len; i++)
+			if (regret[i] > 0) {
+				if (node->actions[i].actionstr != 'l' && node->actions[i].actionstr != 'd')
+					regret[i] *= 5;
 			}
 	}
 	else if (biasid == 4) {
-		for (int i = 0; i < len; i++)
-			if (oriregret[i] > 0) {
-				if (actionstr[i] != 'l' && actionstr[i] != 'd')
-					regret[i] = oriregret[i] * 10;
-				else
-					regret[i] = oriregret[i];
+		for (int i = 0; i < node->action_len; i++)
+			if (regret[i] > 0) {
+				if (node->actions[i].actionstr != 'l' && node->actions[i].actionstr != 'd')
+					regret[i] *= 10;
 			}
 	}
 	double sum = 0;
-	for (int i = 0; i < len; i++) {
+	for (int i = 0; i < node->action_len; i++) {
 		if (regret[i] > 0)
 			sum += regret[i];
 	}
 	if (sum == 0) {//确保后悔值一定有正数
-		return 1.0 / len;
+		return 1.0 / node->action_len;
 	}
 	if (regret[index] > 0)
 		return regret[index] / sum;
